43c6871968ec60290a90eb2f3e790b4fe0d69a15
omergulen <omergulen@std.iyte.edu.tr>
2019-09-21 20:52:22 +0300

    part1 :: course-information step4

--- part1/course-information/src/index.js
+++ part1/course-information/src/index.js
@@ -9,11 +9,11 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <>
-      {props.content.map(el => (
+    <div>
+      {props.parts.map(el => (
         <Part name={el.name} exercises={el.exercises} />
       ))}
-    </>
+    </div>
   )
 }
 
@@ -23,30 +23,32 @@ const Part = (props) => (
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content content={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
