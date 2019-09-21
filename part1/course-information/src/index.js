a9bca422dbcef099e2daf3bbcf1831378e2c5482
omergulen <omergulen@std.iyte.edu.tr>
2019-09-21 20:14:32 +0300

    part1 :: course-information step1

--- part1/course-information/src/index.js
+++ part1/course-information/src/index.js
@@ -1,28 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      {props.content.map(el => (
+        <p>
+          {el.part} {el.exercises}
+        </p>
+      ))}
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
-  const exercises1 = 10
   const part2 = 'Using props to pass data'
-  const exercises2 = 7
   const part3 = 'State of a component'
+  const exercises1 = 10
+  const exercises2 = 7
   const exercises3 = 14
+
+  const content = [
+    { part: part1, exercises: exercises1 },
+    { part: part2, exercises: exercises2 },
+    { part: part3, exercises: exercises3 },
+  ]
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content content={content} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
