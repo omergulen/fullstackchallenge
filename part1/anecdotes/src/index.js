63c38365094a257cc18f127bd633fdec9c7a65d3
omergulen <omergulen@std.iyte.edu.tr>
2019-09-22 17:01:33 +0300

    part1 :: anecdotes step3*

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,22 +1,61 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Anectode = ({ anectode, vote, handleVote }) => (
+  <div>
+    <p>{anectode}</p>
+    <p>has {vote} votes</p>
+    <Button handleClick={handleVote} text='vote' />
+  </div>
+)
+
+const Title = ({ text }) => (<h1>{text}</h1>)
+
+const DailyAnectode = ({ anectode, vote, handleVote, handleNextQuote }) => (
+  <div>
+    <Title text='Anectode of the day' />
+    <Anectode anectode={anectode} vote={vote} handleVote={handleVote} />
+    <Button handleClick={handleNextQuote} text='next anectode' />
+  </div>
+)
+
+const AnectodeWithMostVotes = ({ anectode, vote, handleVote }) => (
+  <div>
+    <Title text='Anectode with most votes' />
+    <Anectode anectode={anectode} vote={vote} handleVote={handleVote} />
+  </div>
+)
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(anecdotes.map(el => 0))
+  const maxIndex = votes.indexOf(Math.max(...votes));
 
   const handleNextQuote = () => setSelected(Math.floor(Math.random() * props.anecdotes.length))
-  const handleVote = () => {
+  const handleVote = (index) => {
     let newVotes = [...votes];
-    newVotes[selected]++;
+    newVotes[index]++;
     setVotes(newVotes);
   }
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-      <button onClick={handleVote}>vote</button>
-      <button onClick={handleNextQuote}>next quote</button>
+      <DailyAnectode
+        anectode={props.anecdotes[selected]}
+        vote={votes[selected]}
+        handleVote={() => handleVote(selected)}
+        handleNextQuote={handleNextQuote}
+      />
+      <AnectodeWithMostVotes
+        anectode={props.anecdotes[maxIndex]}
+        vote={votes[maxIndex]}
+        handleVote={() => handleVote(maxIndex)}
+      />
     </div>
   )
 }
