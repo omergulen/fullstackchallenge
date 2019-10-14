5d0e12184d3927f6de84ea4cf33a5dd4451fbd7c
omergulen <omergulen@std.iyte.edu.tr>
2019-10-14 17:30:11 +0300

    part3 :: Phonebook backend step2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,10 @@ app.get('/api/people', (req, res) => {
   res.json(people)
 })
 
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${people.length} people</p><p>${new Date()}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
